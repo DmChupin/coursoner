30614ed4a1dd4d7990b50de16fcc82c02e099c14
Дмитрий Чупин <dchupin>
2024-01-20 13:01:05 +0300

    feat: fix tabs and styles

--- src/app/shared/components/tabs/tab/tab.component.ts
+++ src/app/shared/components/tabs/tab/tab.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ContentChildren,
+  Input,
+  OnInit,
+  QueryList,
+  inject,
+} from '@angular/core';
 
 @Component({
   selector: 'app-tab',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tab.component.html',
-  styleUrl: './tab.component.scss'
+  styleUrl: './tab.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TabComponent {
+  @Input()
+  active: boolean = false;
+  @Input()
+  tabTittle: string = '';
 
+  private cdr = inject(ChangeDetectorRef);
+  public visibility = false;
+
+  public changeVisibility(visibility: boolean) {
+    this.visibility = visibility;
+    this.cdr.markForCheck();
+  }
 }
--- src/app/shared/components/tabs/tabs.component.ts
+++ src/app/shared/components/tabs/tabs.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ContentChildren,
+  Input,
+  QueryList,
+} from '@angular/core';
 import { TabComponent } from './tab/tab.component';
 
 @Component({
@@ -8,8 +15,29 @@ import { TabComponent } from './tab/tab.component';
   imports: [CommonModule, TabComponent],
   templateUrl: './tabs.component.html',
   styleUrl: './tabs.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TabsComponent {
+export class TabsComponent implements AfterViewInit {
   @Input()
-  tabNames: string[] = [];
+  tabNames: string[] = []; //TODO: перейти на заголовки самих tab
+
+  @ContentChildren(TabComponent)
+  tabs!: QueryList<TabComponent>;
+
+  activeTab: TabComponent | null = null;
+
+  ngAfterViewInit(): void {
+    const activeTab: TabComponent | undefined =
+      this.tabs.find((tab) => tab.active) || this.tabs.get(0);
+
+    if (activeTab) {
+      this.onSelect(activeTab);
+    }
+  }
+  onSelect(tab: TabComponent) {
+    this.tabs.forEach((tab) => {
+      tab.changeVisibility(false);
+    });
+    tab.changeVisibility(true);
+  }
 }
